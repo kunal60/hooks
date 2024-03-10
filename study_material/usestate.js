184b98a56184209e3156c00cbd0dbea1301d43f6
Kunal <kunalmalhotra9322@gmail.com>
2024-03-10 19:19:24 +0100

    use state hook

---
+++ study_material/usestate.js
@@ -0,0 +1,36 @@
+//https://www.geeksforgeeks.org/reactjs-usestate-hook/
+import React, { useState } from 'react';
+import {
+  Text,
+  Button,
+  StyleSheet,
+  SafeAreaView
+} from "react-native";
+
+export default function App() {
+  const [click, setClick] = useState(0);
+
+  const handleButtonClick = () => {
+    setClick(click + 1);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.textContainer}>
+        You clicked {click} times
+      </Text>
+      <Button title="Click me" onPress={handleButtonClick} />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#001f3f',
+  },
+
+  textContainer: {
+    color: 'white',
+  },
+});
